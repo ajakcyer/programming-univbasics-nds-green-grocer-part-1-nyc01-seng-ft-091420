d962cbbf3097f5d21a958ef1b373b53b84cb9081
Ajak Cyer <ajakcyer97@gmail.com>
2020-09-14 03:57:56 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,8 +1,15 @@
+require 'pry'
 def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
 
+  collection.each do |ipc|
+    if ipc[:item] == name
+      return ipc
+    end
+  end
+  return nil
 end
 
 def consolidate_cart(cart)
@@ -11,6 +18,33 @@ def consolidate_cart(cart)
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
 
+  receipt = []
+  name =
+  cart.count do |cart_hashes|
+    cart.count(cart_hashes)
+    cart_hashes[:count] = cart.count(cart_hashes)
+    receipt.
+    
+    # name = cart_hashes[:item]
+    # binding.pry
+    # find_item_by_name_in_collection(name, cart).count(cart_hashes)
+    # binding.pry
+  end
+
+
+
+
+
+
+
+# new_cart = {}
+  
+#   cart.each do |cart_array|          #cart_array = ipc => values
+#     cart_array.each do |cart_key, cart_val| #cart_key = i/p/c    cart_val =
+      
+      
+#     end
+#   end
 end
 
 
